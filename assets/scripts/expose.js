72c3914ed18f83c84250272ea1cff012c19f6156
Sriharsha Kavuri <150100995+kavurisriharsha@users.noreply.github.com>
2025-05-07 01:22:18 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -16,29 +16,29 @@ function init() {
     let selectedHorn = this.value;
 
     if (selectedHorn === 'air-horn') {
-      hornImage.src = '../assets/images/air-horn.svg';
-      hornAudio.src = '../assets/audio/air-horn.mp3';
+      hornImage.src = 'assets/images/air-horn.svg';
+      hornAudio.src = 'assets/audio/air-horn.mp3';
     } else if (selectedHorn === 'car-horn') {
-      hornImage.src = '../assets/images/car-horn.svg';
-      hornAudio.src = '../assets/audio/car-horn.mp3';
+      hornImage.src = 'assets/images/car-horn.svg';
+      hornAudio.src = 'assets/audio/car-horn.mp3';
     } else if (selectedHorn === 'party-horn') {
-      hornImage.src = '../assets/images/party-horn.svg';
-      hornAudio.src = '../assets/audio/party-horn.mp3';
+      hornImage.src = 'assets/images/party-horn.svg';
+      hornAudio.src = 'assets/audio/party-horn.mp3';
     } else {
-      hornImage.src = '../assets/images/no-image.png';
+      hornImage.src = 'assets/images/no-image.png';
       hornAudio.src = '';
     }
   });
 
   volumeSlider.addEventListener('input', function() {
     if (this.value == 0) {
-      volumeIcon.src = '../assets/icons/volume-level-0.svg';
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
     } else if (this.value < 33) {
-      volumeIcon.src = '../assets/icons/volume-level-1.svg';
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
     } else if (this.value < 67) {
-      volumeIcon.src = '../assets/icons/volume-level-2.svg';
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
     } else {
-      volumeIcon.src = '../assets/icons/volume-level-3.svg';
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
     }
     hornAudio.volume = this.value / 100;
   });
