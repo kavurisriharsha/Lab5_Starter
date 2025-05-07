58a4ddb8a4be3ef459f108031599ac005c3b182c
Sriharsha Kavuri <sriharshakavuri1@gmail.com>
2025-05-07 00:32:35 -0700

    part 1 done

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,52 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+test('valid: Balls69', () => {
+  expect(isStrongPassword('Balls69')).toBe(true);
+});
+
+test('valid: balls_69', () => {
+  expect(isStrongPassword('balls_69')).toBe(true);
+});
+
+test('invalid: 69balls', () => {
+  expect(isStrongPassword('69balls')).toBe(false);
+});
+
+test('invalid: b', () => {
+  expect(isStrongPassword('b')).toBe(false);
+});
+
+
+test('valid: 12/12/2020', () => {
+  expect(isDate('12/12/2020')).toBe(true);
+});
+
+test('valid: 1/1/2020', () => {
+  expect(isDate('1/1/2020')).toBe(true);
+});
+
+test('invalid: 12/12/20', () => {
+  expect(isDate('12/12/20')).toBe(false);
+});
+
+test('invalid: 12/12/20200', () => {
+  expect(isDate('12/12/20200')).toBe(false);
+});
+
+test('valid: #000', () => {
+  expect(isHexColor('#000')).toBe(true);
+});
+
+test('valid: abcdef', () => {
+  expect(isHexColor('abcdef')).toBe(true);
+});
+
+test('invalid: #0000', () => {
+  expect(isHexColor('#0000')).toBe(false);
+});
+
+test('invalid: ggg', () => {
+  expect(isHexColor('ggg')).toBe(false);
+});
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,54 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  let hornSelect = document.getElementById('horn-select');
+  let hornImage = document.querySelector('#expose img');
+  let hornAudio = document.querySelector('#expose audio');
+  let volumeSlider = document.getElementById('volume');
+  let volumeIcon = document.querySelector('#volume-controls img');
+  let playButton = document.querySelector('button');
+
+  hornSelect.addEventListener('change', function() {
+    let selectedHorn = this.value;
+
+    if (selectedHorn === 'air-horn') {
+      hornImage.src = '../assets/images/air-horn.svg';
+      hornAudio.src = '../assets/audio/air-horn.mp3';
+    } else if (selectedHorn === 'car-horn') {
+      hornImage.src = '../assets/images/car-horn.svg';
+      hornAudio.src = '../assets/audio/car-horn.mp3';
+    } else if (selectedHorn === 'party-horn') {
+      hornImage.src = '../assets/images/party-horn.svg';
+      hornAudio.src = '../assets/audio/party-horn.mp3';
+    } else {
+      hornImage.src = '../assets/images/no-image.png';
+      hornAudio.src = '';
+    }
+  });
+
+  volumeSlider.addEventListener('input', function() {
+    if (this.value == 0) {
+      volumeIcon.src = '../assets/icons/volume-level-0.svg';
+    } else if (this.value < 33) {
+      volumeIcon.src = '../assets/icons/volume-level-1.svg';
+    } else if (this.value < 67) {
+      volumeIcon.src = '../assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = '../assets/icons/volume-level-3.svg';
+    }
+    hornAudio.volume = this.value / 100;
+  });
+
+  playButton.addEventListener('click', function() {
+    hornAudio.play();
+    console.log(hornAudio.volume);
+    console.log("test");
+    if (hornSelect.value === 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
+    
+    
 }
